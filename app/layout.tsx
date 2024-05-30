afb7fd4edde17bb1dc8055518be810d8e5b9a053
blueice01 <wkdma1524@gmail.com>
2024-05-30 13:06:33 +0900

    fix errors wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Judson, Libre_Franklin } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const judson = Judson({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: "400"
+})
+
+const libre_franklin = Libre_Franklin({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: "400"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${judson.className} ${libre_franklin.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
